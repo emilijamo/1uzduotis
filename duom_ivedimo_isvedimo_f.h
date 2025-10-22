f7e3f1db6f22a9f8216e862737940c2aaee6f3ee
emilijamo <emilija.morkunaite0@gmail.com>
2025-10-22 20:24:12 +0300

    Update duom_ivedimo_isvedimo_f.h
    Funkcijos failo_nuskaitymas bei rezultatu_isvedimas padarytos šabloninėmis funkcijomis

--- duom_ivedimo_isvedimo_f.h
+++ duom_ivedimo_isvedimo_f.h
@@ -8,7 +8,12 @@ using std::string;
 using std::vector;
 
 void generuoti_faila(string failo_pav, int studsk, int ndsk);
-void failo_nuskaitymas(string fpav, vector<Studentas>& Grupe);
-void rezultatu_isvedimas(string failo_vardas, vector<Studentas>& Grupe, int rez_pasirinkimas);
+
+template <typename Konteineris>
+void failo_nuskaitymas(string fpav, Konteineris& Grupe);
+
+template <typename Konteineris>
+void rezultatu_isvedimas(string failo_vardas, Konteineris& Grupe, int rez_pasirinkimas);
+
 
 Studentas ivesk();
