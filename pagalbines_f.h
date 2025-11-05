64c7e02e7f19539b3d94bfcba53bf6bab7a70af4
emilijamo <emilija.morkunaite0@gmail.com>
2025-11-05 12:13:35 +0200

    Update pagalbines_f.h
    Pridėtas apibrėžimas std::vector bei std::list užimamos atminties funkcijos

--- pagalbines_f.h
+++ pagalbines_f.h
@@ -2,11 +2,18 @@
 
 #include <string>
 #include <random>
+#include <vector>
+#include <list>
 
 using std::string;
+using std::vector;
+using std::list;
 
 bool ar_skaicius(string reiksme);
 int skaiciaus_ivedimas(string prasymas, int min=INT_MIN, int max=INT_MAX);
+
+size_t atminties_naudojimas_vector(const vector<Studentas>& Grupe);
+size_t atminties_naudojimas_list(const list<Studentas>& Grupe);
 
 class RandInt {
 public:
@@ -16,4 +23,5 @@ private:
     std::random_device rd;
     std::mt19937 mt;
     std::uniform_int_distribution<int> dist;
+
 };
