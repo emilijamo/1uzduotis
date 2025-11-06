0c72e21905efc2daabcb1c0bfcecb157495968f3
emilijamo <emilija.morkunaite0@gmail.com>
2025-11-06 11:41:01 +0200

    Add isvesti_studentus function template declaration
    refactoringas

--- duom_ivedimo_isvedimo_f.h
+++ duom_ivedimo_isvedimo_f.h
@@ -15,5 +15,8 @@ void failo_nuskaitymas(string fpav, Konteineris& Grupe);
 template <typename Konteineris>
 void rezultatu_isvedimas(string failo_vardas, Konteineris& Grupe, int rez_pasirinkimas);
 
+template <typename Konteineris>
+void isvesti_studentus (Konteineris& Grupe,Konteineris& vargsiukai, Konteineris& kietiakai, int rez_pasirinkimas);
 
 Studentas ivesk();
+
